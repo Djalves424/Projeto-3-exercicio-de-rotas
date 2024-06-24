f68c4548d76764e374605a830348c415a1cd997e
Djalves424 <djalves424@gmail.com>
2024-06-24 15:50:02 -0300

    Ex3 finish

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,24 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import Home from "./routes/Home";
 import HomeBody from "./routes/Home/HomeBody";
 import Promotion from "./routes/Home/Promotion";
+import Subscription from "./routes/Subscription";
+
 
 function App() {
   
-    return (
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} >
-            <Route index element={<HomeBody />} />
-            <Route path="promotion" element={<Promotion />} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
-    );
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} >
+          <Route index element={<Navigate to="/home" />} />
+          <Route path="home" element={<HomeBody />} />
+          <Route path="promotion" element={<Promotion />} />
+          <Route path="sub" element={<Subscription />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App
--- src/routes/Subscription/index.tsx
+++ src/routes/Subscription/index.tsx
@@ -1,18 +1,19 @@
 import { Link } from "react-router-dom";
-import Button from "../../../components/Button";
-import Card from "../../../components/Card";
+import Card from "../../components/Card";
+import Button from "../../components/Button";
 
-export default function Promotion() {
+
+export default function Subscription() {
   return (
     <main>
       <section>
         <div className="container">
           <div className="mt30 mb30">
-            <Card title="Página de promoção" />
+            <Card title="Página de inscrição" />
           </div>
           <div className="dflex">
-            <Link to="/sub">
-              <Button text="Quero participar" />
+            <Link to="/promotion">
+              <Button text="Ver promoção" />
             </Link>
           </div>
         </div>
